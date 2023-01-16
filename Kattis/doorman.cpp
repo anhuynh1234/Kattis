f2f57e78ec04f9dba8b5494caaeda595c65deb75
An Huynh <110524240+anhuynh1234@users.noreply.github.com>
2023-01-15 23:22:52 -0700

    addition

---
+++ Kattis/doorman.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+int main(){
+    int x;
+    std::string lines;
+    std::cin >> x >> lines;
+    int male, female, diff = 0;
+    
+
+    while(diff <= x){
+        
+    }
+
+}
