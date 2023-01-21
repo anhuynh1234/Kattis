df6c7b86094cb11d2848814214153342d9461603
An Huynh <110524240+anhuynh1234@users.noreply.github.com>
2023-01-21 00:56:47 -0700

    changes

---
+++ Kattis/kolone.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+int main(){
+    int n1, n2, t;
+    std::cin >> n1 >> n2;
+    std::string left, right, lines;
+
+    std::cin >> left >> right;
+    std::cin >> t;
+    std::reverse(left.begin(), left.end());
+
+    lines = left + right;
+
+    for (int i = 0; i < t; i++){
+        
+    }
+
+}
